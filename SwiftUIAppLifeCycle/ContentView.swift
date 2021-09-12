feca504dcd1b313d7b089bc17fd3cb583b57cabb
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2021-09-12 20:18:13 +0700

    Initial Commit

---
+++ SwiftUIAppLifeCycle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAppLifeCycle
+//
+//  Created by Arifin Firdaus on 12/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAppLifeCycle/SwiftUIAppLifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppLifeCycleApp.swift
+//  SwiftUIAppLifeCycle
+//
+//  Created by Arifin Firdaus on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppLifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
